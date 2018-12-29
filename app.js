0f9e85d819601d111c80bfb6a706a06c7aa3adf7
fijalkovsky <lilka@windowslive.com>
2018-12-29 17:37:54 +0100

    mongodb and mongoose

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/restful_blog_app");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
 app.get('/', function(req, res){
-	res.render("index");
+	Blog.find({}, function(err, blogs){
+		if(err) {
+			console.log("Error!");
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	});
+	
 });
 
 app.listen(3000, function() {
